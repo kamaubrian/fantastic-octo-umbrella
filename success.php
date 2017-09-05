8f1493387dfb37dddc005806e5e7905f3e203c6c
mtotodev05 <mtotodev05@gmail.com>
2017-09-05 17:08:54 +0300

    Configured Success

--- success.php
+++ success.php
@@ -1,13 +1,25 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: brian-kamau
- * Date: 9/5/17
- * Time: 4:37 PM
- */
+<html>
+<head>
+
+    <link rel="stylesheet" href="css/icons.css">
+    <link rel="stylesheet" href="css/components.css">
+    <link rel="stylesheet" href="css/responsee.css">
+
+</head>
+<body>
+
+<div class="center">
+    <center>
+    <div>
+        <h2>Message Sent Successfully</h2>
+        <br><br>
+       <a href="index.html">Go Home</a>
+    </div>
+    </center>
+</div>
 
 
+</body>
 
 
-
-?>
+</html>
